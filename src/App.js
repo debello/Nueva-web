2eb0cb37fb1f5b05951669de34681a8577c4bccf
Santi Bello <santidebello@gmail.com>
2019-11-25 15:15:45 +0100

    Old to New Web

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { Titulo, Descripcion, Destrezas, Proyectos, Pie } from "./Portfolio";
 import './App.css';
+//import {Hola} from "./test"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Henlo, this is React.</p>
+      <Titulo />
+      <Descripcion />
+      <Destrezas />
+      <Proyectos />
+      <Pie />
     </div>
   );
 }
---
+++ src/test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Hola = () => (
+    <p>hola</p>
+)
+export {Hola}
