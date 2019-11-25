1373ff375d8c170825bcaafedcd66490825d45a9
Santi Bello <santidebello@gmail.com>
2019-11-25 18:24:54 +0100

    Intenta insertar script que reescriba el nombre h1

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import "./old.css"
 
+const script = document.createElement("script")
+script.async = true
+script.src = "/scripts/myscript.js"
+document.body.appendChild(script)
+
 const Titulo = () => {
+    
     return (
         <div>
+        <script />
             <nav id='navbar'>
                 <a href='#quien-soy'>Quién soy</a>
                 <a href='#destrezas'>Destrezas</a>
@@ -13,11 +20,11 @@ const Titulo = () => {
             <div id='raya_primera'></div>
             <div id='mini-raya'></div>
             <div id='welcome-section'>
-                <h1 >SANTI BELLO</h1>
+                <h1 id="this-is-title" >SANTI BELLO</h1>
                 <h2 className='hover-1'>Desarrollador Web.</h2>
                 <h2 className='hover-2'>* JavaScript</h2>
                 <h2 className='hover-2'>* PHP</h2>
-                <h2 className='hover-2'>{"{Actualmente empleado en React}"}</h2>
+                <h2 className='hover-2'>{"{Trabajando en React}"}</h2>
             </div>
         </div>
     )
@@ -28,29 +35,23 @@ const Descripcion = () => {
         <div className='container' id='quien-soy'>
             <div className='jumbotron' style={{lineHeight: "20px"}} >
                 <div id='txt'>
-
                     <p className='rwd-line'>Soy un estudiante <span className='hover-2'>autodidacta</span> en Galicia. Programo a diario.
                     </p><br />
                     <p className='rwd-line'>Hace más de un año empecé a trastear con Java después de verle las
                     'tripas' a un videojuego, lo que me llevaría a descubrir la programación Web.
                     <br></br>
-                    Me llamó la atención el <span className='hover-2'>valor</span> que podemos aportar mediante un código y un objetivo conjunto.
-                    </p>
+                    Me llamó la atención el <span className='hover-2'>valor</span> que podemos aportar mediante un código y un objetivo conjunto.</p>
                     <br /><br />
                     <div id='pie-pagina'>
-                        <img className="heart" src="img/heart_red.png" alt="corazón rojo" />
-                    </div>
+                        <img className="heart" src="img/heart_red.png" alt="corazón rojo" /></div>
                     <br /><br /><br />
                     <p className='rwd-line'>Actualmente <strike>busco un trabajo que permita seguir formándome y
-                        abrirme a nuevas posibilidades</strike> estoy empleado en JavaScript y React :)
-                    </p>
+                        abrirme a nuevas posibilidades</strike> estoy empleado en JavaScript y React :)</p>
                     <br />
                     <p className='rwd-line'>Me considero <span className='hover-2'>responsable</span>, organizado, trabajo en equipo y social,
                     habiendo participado en la planificación y ejecución de más de <span className='hover-2'>140 eventos</span> de ocio a nivel
-                    regional desde 2014 en dos organizaciones diferentes.
-                    </p>
-                    <p className='rwd-line'>Contáctame vía gmail para cualquier duda, puesta a prueba o saludarme!
-                    </p>
+                    regional desde 2014 en dos organizaciones diferentes.</p>
+                    <p className='rwd-line'>Contáctame vía gmail para cualquier duda, puesta a prueba o saludarme!</p>
                 </div>
             </div>
         </div>
@@ -104,7 +105,7 @@ const Proyectos = () => {
         <>
             <div id='projects-redirect'></div>
             <h2>Proyectos.</h2>
-            <section id='projects' class='projects-grid'>
+            <section id='projects' className='projects-grid'>
                 <a className='p-1' href='https://debello.github.io/todolist/' rel="noopener noreferrer">To do List </a>
                 <a className='p-2' href='https://debello.github.io/kanban_bello/' rel="noopener noreferrer">Kanban </a>
                 <a className='p-3' href='https://github.com/debello/API-Clima' rel="noopener noreferrer">API Clima</a>
@@ -120,8 +121,8 @@ const Pie = () => {
         <div id='pie-pagina'>
             <p id='profile-link'><strong>Connect</strong></p>
             <div id='social-buttons'>
-                <a href="https://twitter.com/debellodev" class="" rel="noopener noreferrer"><img className="RRSS" src="img/twitter-logo.png"alt="twitter handle" /></a>
-                <a href="https://github.com/debello/" class="" rel="noopener noreferrer"><img className="RRSS" src="img/github-logo.png" alt="twitter handle" /></a>
+                <a href="https://twitter.com/debellodev" className="" rel="noopener noreferrer"><img className="RRSS" src="img/twitter-logo.png"alt="twitter handle" /></a>
+                <a href="https://github.com/debello/" className="" rel="noopener noreferrer"><img className="RRSS" src="img/github-logo.png" alt="twitter handle" /></a>
             </div>
             <div id='mini-raya'></div>
             <p><strong>Contact</strong></p>
