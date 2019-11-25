3168a5ea75d809a0828f9d5cd7f9d11aa7c27ca6
Santi Bello <santidebello@gmail.com>
2019-11-25 18:30:51 +0100

    Updates

---
+++ precache-manifest.70431a5cd4505d0b0bddecd6cdb7f658.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "538d042792a73c7eebfd4970e5297c76",
+    "url": "/Nueva-web/index.html"
+  },
+  {
+    "revision": "727d273f0a36b1cbc196",
+    "url": "/Nueva-web/static/css/main.8a4468c4.chunk.css"
+  },
+  {
+    "revision": "110b290a590a80e4b9d4",
+    "url": "/Nueva-web/static/js/2.874378e6.chunk.js"
+  },
+  {
+    "revision": "727d273f0a36b1cbc196",
+    "url": "/Nueva-web/static/js/main.17996658.chunk.js"
+  },
+  {
+    "revision": "8228a8dc189bd911614a",
+    "url": "/Nueva-web/static/js/runtime-main.16f8a5bb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Nueva-web/precache-manifest.e10cf5051d1b6d3b5a38c6a2d4ea9bc0.js"
+  "/Nueva-web/precache-manifest.70431a5cd4505d0b0bddecd6cdb7f658.js"
 );
 
 self.addEventListener('message', (event) => {
