c54aace2c1d52ba9bab7630eca75801a64dd61c9
Santi Bello <santidebello@gmail.com>
2020-03-10 03:41:40 +0100

    Add button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components'
 import { Titulo, Descripcion, Destrezas, Proyectos, Pie } from "./parts/Portfolio";
 import './css/old.css'
 console.log("test")
 console.log(window.location)
+
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const NavBlock = () => {
+    switch(count) {
+      case 0: 
+        return <Titulo />
+      case 1:
+        return <Descripcion />
+      case 2:
+        return <Destrezas />
+      case 3:
+        return <Proyectos />
+      case 4:
+        return <Pie />
+      default: 
+        setCount(0)
+    }
+  }
+
   return (
     <Container className="container">
-      
-      <Titulo />
-      <Descripcion />
-      <Destrezas />
-      <Proyectos />
-      <Pie />
+      {console.log(count)}
+      <button className="moveBlock" onClick={() => setCount(count+1)}>Click me!</button>
+      {NavBlock()}
+
     </ Container>
   );
 }
 
 const Container = styled.div`   
   font-size: 1.4rem;
-
+    .moveBlock {
+      width: 100px;
+      height: 100px;
+    }
     display: grid;
     grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
     grid-row-gap: 400px;
