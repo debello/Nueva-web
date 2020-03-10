9c11ec8a01c48585407cb63e94ba146418611b03
Santi Bello <santidebello@gmail.com>
2020-03-10 04:28:14 +0100

    refactor & clean unused lines

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import styled from 'styled-components'
 import { Titulo, Descripcion, Destrezas, Proyectos, Pie } from "./parts/Portfolio";
 import './css/old.css'
-console.log("test")
-console.log(window.location)
 
 const App = () => {
   const [count, setCount] = useState(0)
@@ -30,55 +28,31 @@ const App = () => {
   return (
     <Container className="container">
       {console.log(count)}
-      <button className="moveBlock" onClick={() => setCount(count+1)}>Click me!</button>
+      <div>
+        <button className="move_block_left" onClick={() => setCount(count-1)}>Click me!</button>
+        <button className="move_block_right" onClick={() => setCount(count+1)}>Click me!</button>
+      </div>
       {NavBlock()}
-
     </ Container>
   );
 }
 
 const Container = styled.div`   
   font-size: 1.4rem;
-    .moveBlock {
+  display: grid;
+  grid-template-rows: 100px 1fr 1fr;
+  grid-row-gap: 50px;
+
+    .move_block_left {
       width: 100px;
       height: 100px;
+      float: left;
+    } 
+    .move_block_right {
+      width: 100px;
+      height: 100px;
+      float: right;
     }
-    display: grid;
-    grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
-    grid-row-gap: 400px;
-  
-`
-/*
-class App extends React.Component {
-  state = { count: 0 }
+`   
 
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 })
-    console.log(this.state.count)
-  }
-  
-  handleDecrement = () => {
-    this.setState({ count: this.state.count - 1 })
-  }
-
-  cambiazo = () => {
-    if(this.state.count > 0) {
-      return <Descripcion />
-    }
-  }
-  render() {
-    return (
-      <Container className="container">
-        
-        <Titulo  onClick={this.handleIncrement} />       
-        <button onClick={this.handleIncrement}>Click me!</button>
-        {this.cambiazo()}
-        <Destrezas />
-        <Proyectos />
-        <Pie />
-      </ Container>
-    );
-  }
-}
-*/ 
 export default App;
