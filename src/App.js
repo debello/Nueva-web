a7db70e5f36be35717b73db0902af9c64f9cd6e2
debello <42223093+debello@users.noreply.github.com>
2020-03-25 01:37:19 +0100

    Add onMouseOver

--- src/App.js
+++ src/App.js
@@ -5,34 +5,34 @@ import './css/old.css';
 
 const App = () => {
   const [count, setCount] = useState(1)
-  
-  const NavBlock = () => {
-    switch(count) {
-      case 0:
-        return setCount(5)
-      case 1:
-        return <Titulo />
-      case 2:
-        return <Descripcion />
-      case 3:
-        return <Destrezas />
-      case 4:
-        return <Proyectos />
-      case 5:
-        return <Pie />
-      case 6: 
-        return setCount(1)
-      default: 
-        console.log("Error: default returned")
-    }
+
+  const NavBlock = () => {  
+      switch(count) {
+        case 0:
+          return setCount(5)
+        case 1:
+          return <Titulo />
+        case 2:
+          return <Descripcion />
+        case 3:
+          return <Destrezas />
+        case 4:
+          return <Proyectos />
+        case 5:
+          return <Pie />
+        case 6: 
+          return setCount(1)
+        default: 
+          console.log("Error: default returned")
+      }
   }
 
   return (
     <Container className="container">
       {console.log(count)}
-        <button className="move_block_left" onClick={() => setCount(count-1)}>Click me!</button>
+        <div className="move_block_left" onMouseOver={() => setCount(count-1)} >Click me!</div>
         {NavBlock()}
-        <button className="move_block_right" onClick={() => setCount(count+1)}>Click me!</button>
+        <div className="move_block_right" onMouseOver={() => setCount(count+1)} >Click me!</div>
     </ Container>
   );
 }
@@ -44,14 +44,6 @@ const Container = styled.div`
   grid-template-columns: 0.25fr 1fr 0.25fr;
   grid-column-gap: 10%;
   justify-content: center;
-
-    .move_block_left {
-      
-    } 
-    .move_block_right {
-
-
-    }
 `   
 
 export default App;
